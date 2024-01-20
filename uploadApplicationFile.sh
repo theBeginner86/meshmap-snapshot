afdaa50261193f0921fe2900e422dbb7c2ba142e
Pranav Singh <pranavsingh02@hotmail.com>
2024-01-21 03:28:54 +0530

    Update uploadApplicationFile.sh

--- uploadApplicationFile.sh
+++ uploadApplicationFile.sh
@@ -9,7 +9,7 @@ ls
 cat  __intermediate_file.yml
 echo "hello"
 # MESHERY_PATTERN_FILE=$(awk '{ gsub(/"/, "\\\"", $0); printf "%s\\n", $0}' __intermediate_file.yml)
-MESHERY_PATTERN_FILE=$(pattern_file=$(cat __intermediate_file.yml) node ./action/normalize/index.js)
+MESHERY_PATTERN_FILE=$(pattern_file="__intermediate_file.yml" node ./action/normalize/index.js)
 
 # # convert to uri-encoded str
 UPLOAD_TYPE=$(printf %s "$UPLOAD_TYPE" | jq -sRr @uri)
